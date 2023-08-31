b81c3dfd88ddb4fccaba99e65f873de01ccc0f82
Gregorio <Greg.del801@gmail.com>
2023-08-31 11:41:09 -0700

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { image } from "../data/data";
 
+const divStyle = {
+  background: "red"
+ 
+
+}
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>
+      About Me
+    </h2>
+    <p>
+      my choosing
+    </p>
+    <img src={image} alt="I made this"></img>
+  </div>
+  )
 }
 
 export default About;
